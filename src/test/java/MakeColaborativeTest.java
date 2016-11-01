71da0fa3bf7476c1acfc196b8a8d929386955814
Daniel Andres Ayala <daniel.ayala@mail.escuelaing.edu.co>
2016-11-01 09:54:20 -0500

    Despliegue

---
+++ src/test/java/MakeColaborativeTest.java
@@ -0,0 +1,37 @@
+
+
+import java.util.List;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 2100609
+ */
+public class MakeColaborativeTest {
+
+    public MakeColaborativeTest() {
+    }
+    @Before
+    public void setUp() {
+    }
+
+
+    
+    @Test
+    public void PrimerPrueba() {
+       
+        assertEquals(true, true);
+        
+    }
+    
+    
+}
